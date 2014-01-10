c5ba0f7cae61d144ab79e4979692cd4a640444f4
Nikita Bayev <nikita@bayev.kz>
2014-01-10 14:01:42 +0600

    Created Grunt config for CSS/HTML minification

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = function (grunt) {
+	grunt.initConfig({ 
+        watch: {
+          css: {
+              files: ['styles.css'],
+              tasks: ['cssmin']
+          }  
+        },
+        autoprefixer: {
+          css: {
+              src: 'styles.css',
+              dest: 'styles.prefixed.css'
+          }  
+        },
+        cssmin: {
+          combine: {
+            files: {
+              'styles.min.css': ['styles.prefixed.css']
+            }
+          }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    useShortDoctype: true,
+                    removeRedundantAttributes: true,
+                    removeAttributeQuotes: true,
+                    collapseBooleanAttributes: true
+                },
+                files: {
+                    'index.html': 'index.src.html'
+                }
+            },
+        }
+	});
+    
+	grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.registerTask('default', ['autoprefixer', 'cssmin', 'htmlmin', 'watch']);
+};
